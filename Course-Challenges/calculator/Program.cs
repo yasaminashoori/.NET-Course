41c9baa77050f7238fea6709cfb4c560f7b1e5d1
yasamin ashoori <yasaminashoori18@gmail.com>
2022-10-31 14:22:37 +0330

    add new cw to calculator and try catch

--- Course-Challenges/calculator/Program.cs
+++ Course-Challenges/calculator/Program.cs
@@ -7,30 +7,40 @@ namespace calculator
         static void Main(string[] args)
         {
             CalcOperations calcOperations = new CalcOperations();
-            Console.WriteLine("Welcome to basic OOP calculator! Enter a number. {0}1 - Addition{0}2 - Substraction{0}3 - Multiplication{0}4 - Division", Environment.NewLine);
-
+            Console.Clear();
+            Console.WriteLine("--------------");
+            Console.WriteLine("Calculator Program");
+            Console.WriteLine("--------------");
+            Console.WriteLine("Enter number : {0}1 - Addition{0}2 - Substraction{0}3 - Multiplication{0}4 - Division", Environment.NewLine);
 
             var userInput = int.Parse(Console.ReadKey().KeyChar.ToString());
             Console.WriteLine();
 
             GetUserInput(calcOperations);
 
-            var getOperation = new double[]{
-                           calcOperations.Counting(),
-                           calcOperations.Subtraction(),
-                           calcOperations.Multiplication(),
-                           calcOperations.Division()
-    };
+            var getOperation = new double[] { calcOperations.Counting(), calcOperations.Subtraction(), calcOperations.Multiplication(), calcOperations.Division() };
             Console.WriteLine(getOperation[userInput - 1]);
-
             Console.ReadKey();
         }
         static void GetUserInput(CalcOperations calcOperations)
         {
-            Console.WriteLine("Enter the 1. number: ");
-            calcOperations.firstNum = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the 2. number: ");
-            calcOperations.secondNum = double.Parse(Console.ReadLine());
+            
+            try
+            {
+                Console.WriteLine("Enter the 1. number: ");
+                calcOperations.firstNum = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the 2. number: ");
+                calcOperations.secondNum = double.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please Entered one number between 1 - 4");
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You can't devide by 0 :( Enter another number !");
+            }
         }
     }
 
