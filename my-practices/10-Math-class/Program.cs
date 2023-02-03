402f970c49646c0bd33972347d3b7f9e0534fd70
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-03 21:58:35 +0330

    Introduction (season 1) practices

--- my-practices/10-Math-class/Program.cs
+++ my-practices/10-Math-class/Program.cs
@@ -7,35 +7,39 @@ namespace math
         static void Main(string[] args)
         {
 
-            int number2 = -10;
+            int number2 = 10;
+            double result = Math.Pow(number2, 2);
+            Console.WriteLine(result);
+
             double result2 = Math.Pow(number2, 3);
             Console.WriteLine(result2);
 
-            double number = Math.Pow(number2, 2);
-            Console.WriteLine(number);
-            // double number means that the result of the Pow is double type although 3 is int
-
-
             int number4 = 36;
-            int result3 = (int) Math.Sqrt(number4);
+            int result3 = (int)Math.Sqrt(number4);
             Console.WriteLine(result3);
-            // sqrt result is a double type but we convert the Math method to int
 
             double number3 = 32.56;
 
-            double result = Math.Abs(number2);
+            double result0 = Math.Abs(number2);
+            Console.WriteLine(result0);
 
             double result4 = Math.Round(number3);
+            Console.WriteLine(result4);
 
             double result5 = Math.Ceiling(number3);
+            Console.WriteLine(result5);
 
             double result6 = Math.Floor(number3);
-
+            Console.WriteLine(result6);
+            
             double result7 = Math.Max(number3, number2);
+            Console.WriteLine(result7);
+
             double result8 = Math.Min(number3, number2);
+            Console.WriteLine(result8);
 
             double result9 = Math.PI;
-            // const of PI
+            Console.WriteLine(result9);
         }
     }
 }
--- my-practices/9-Arithmetics-Operators/Program.cs
+++ my-practices/9-Arithmetics-Operators/Program.cs
@@ -7,19 +7,28 @@ namespace ArithmeticsOperators
         static void Main(string[] args)
         {
             int number = 3;
+            
             number += 2;
+            number++;
             Console.WriteLine(number);
 
             number = number + 6;
+            number--;
             Console.WriteLine(number);
 
             number = number / 2;
             number /= 2;
             Console.WriteLine(number);
 
+            number = number % 2;
+            number %= 2;
+            Console.WriteLine(number);
 
-            // odd or even number with usage of % 
-
+            number = number * 2;
+            number *= 2;
+            Console.WriteLine(number);
+            
+            // odd or even number with % 
             Console.WriteLine("Enter your number : ");
             double numberPercent = Convert.ToDouble(Console.ReadLine());
             numberPercent %= 2;
@@ -31,12 +40,6 @@ namespace ArithmeticsOperators
             {
                 Console.WriteLine("this is an odd number !");
             }
-
-
-            number++;
-            number--;
-            Console.WriteLine(number);
-            
         }
     }
 }
