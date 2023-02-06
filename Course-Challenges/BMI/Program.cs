32febee46dfd51289ae1638a61add72a4074a8c8
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-06 13:14:36 +0330

    Resolved Bmi with methods

--- Course-Challenges/BMI/Program.cs
+++ Course-Challenges/BMI/Program.cs
@@ -6,7 +6,13 @@ namespace BMI
     {
         static void Main(string[] args)
         {
+            InputBmi();
+            var result = BmiFormula();
+            Console.WriteLine(result);
+        }
 
+        static void InputBmi()
+        {
             Console.WriteLine("Enter your height in cm :");
 
             var height = Convert.ToDouble(Console.ReadLine());
@@ -14,30 +20,35 @@ namespace BMI
             Console.WriteLine("Enter your weight in kg:");
 
             var weight = Convert.ToDouble(Console.ReadLine());
+        }
 
+        static double BmiFormula(double weight = 160, double height = 80)
+        {
             var bmi = Math.Round(weight / Math.Pow(height / 100, 2), 2);
 
-        
             if (bmi <= 18.4)
             {
-                Console.WriteLine($"BMI = {bmi} You are Underweight");
+                Console.Write("You are Underweight : ");
+                return bmi;
             }
 
             else if (bmi >= 18.52 && bmi <= 24.9)
             {
-                Console.WriteLine($"BMI = {bmi} You are Normal");
+                Console.Write("You are Normal : ");
+                return bmi;
             }
 
             else if (bmi >= 25.0 && bmi <= 39.9)
             {
-                Console.WriteLine($"BMI = {bmi} You are Overweight");
+                Console.Write("You are Overweight : ");
+                return bmi;
             }
 
             else
             {
-                Console.WriteLine($"BMI = {bmi} You are Obese");
+                Console.Write("You are Obese : ");
+                return bmi;
             }
-
 
         }
     }
