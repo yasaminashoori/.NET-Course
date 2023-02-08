a460e40741ff71fc2792e605fabf532d67974a73
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-08 15:07:24 +0330

    Second solution added

--- my-practices/1-Best-Practices/Count-Odd-Numbers(leetcode)/Program.cs
+++ my-practices/1-Best-Practices/Count-Odd-Numbers(leetcode)/Program.cs
@@ -10,31 +10,12 @@ namespace Count_Odd_Numbers_leetcode_
             var low = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter a number more than low as high number : ");
             var high = Convert.ToInt32(Console.ReadLine());
-            bool status = false;
-            if (high >= low)
+            // var between = high - low + 1;
+            int[] nums = new int[high - low + 1];
+            for (int i = 0; i < 10; i++)
             {
-                while (status == false)
-                {
-                    int[] numbers = new int[high - low];
-                    numbers[0] = low;
-                    numbers[1] = low + 1;
-                    numbers[2] = low + 2;
-                    numbers[3] = low + 3;
-                    // low = low + 1;
-                    foreach (var item in numbers)
-                    {
-                        if (numbers.Length / 2 != 0)
-                        {
-                            Console.WriteLine($"The final result for Count Odd numbers : {item}");
-                            // Console.WriteLine(item);
-                        }
-                        else
-                        {
-                            Console.WriteLine("There is no odd number.");
-                        }
-                    }
-                    status = true;
-                }
+                Console.WriteLine(nums[i] = low);
+                ++low;
             }
         }
     }
