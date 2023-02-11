11422cde8be556d26f2e2ba4454579303833c2ad
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-11 19:13:32 +0330

    Updated LINQ (Query)

---
+++ my-practices/61-LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _61_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentIds = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var result = from studentId in studentIds
+                         where (studentId % 2) == 0
+                         select studentId;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            var studentsList = StudentDatabase.GetStudentsFromDb();
+            var studentsScoreMoreThan80 = from student in studentsList
+                                          where student.Score > 80
+                                          select student.StudentName;
+            foreach (var item in studentsScoreMoreThan80)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ my-practices/61-LINQ/Student.cs
@@ -0,0 +1,15 @@
+using System.Linq;
+namespace _61_LINQ
+{
+    internal class Student
+    {
+        public Student()
+        {
+        }
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Score { get; set; }
+        public string StudentCity { get; set; }
+
+    }
+}
