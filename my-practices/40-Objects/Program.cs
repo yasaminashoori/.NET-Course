926209abf506295ce59a6c6b60f8df4f1e28d359
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-11 19:04:01 +0330

    Updated third class part

--- my-practices/40-Objects/Program.cs
+++ my-practices/40-Objects/Program.cs
@@ -9,10 +9,10 @@ namespace _40_Objects
             var myStudents1 = new Students();
             myStudents1.firstName = "Zahra";
             myStudents1.lessons = 17;
+            myStudents1.age = 25;
             myStudents1.GetPersonalInfo();
             myStudents1.SemesterInfo();
-
-            
+    
             var myStudents2 = new Students();
             myStudents2.firstName = "Fathemeh";
             myStudents2.lastName = "Babaei";
