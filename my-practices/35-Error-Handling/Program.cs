91b0406c367b2231e27c73922b493030f598a0c3
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-11 19:01:39 +0330

    Updated try/catch

---
+++ my-practices/35-Error-Handling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _35_Error_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name :");
+            try
+            {
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter your password :");
+                double password = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"your name is {name} \n password : {password}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter an inteeger password and string names");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
