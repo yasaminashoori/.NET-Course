8051075c85549ccf7e27b30df788d7fc4d063432
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-11 19:09:22 +0330

    Updated obj / lists

---
+++ my-practices/53-Objects-In-Lists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _53_ObjectsInLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var plantsLists = new List<Plants>();
+            plantsLists.Add(new Plants("Rose", "flower"));
+            plantsLists.Add(new Plants("sunflower", "flower"));
+            plantsLists.Add(new Plants("tulip", "flower"));
+            plantsLists.Add(new Plants("poppyflower", "flower"));
+
+            foreach (var plant in plantsLists)
+            {
+                Console.WriteLine(plant);
+            }
+        }
+
+        class Plants
+        {
+            public string Name { get; set; }
+            public string Kind { get; set; }
+
+            public Plants(string name, string kind)
+            {
+                Name = name;
+                Kind = kind;
+            }
+
+            public override string ToString()
+            {
+                return $"The name of plant is : {Name}  and kind : {Kind}";
+            }
+        }
+    }
+
+}
