bf9ccd57c0cea5df49c497c8d44770c43b826950
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-11 19:05:10 +0330

    Updated on abstract class

---
+++ my-practices/44-abstract-classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _44_abstract_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus1 = new Bus();
+        }
+        abstract class Vehicle
+        {
+            string name;
+            public void Move()
+            {
+                Console.WriteLine($" {name} is moving right now ! ");
+            }
+        }
+        sealed class Bus : Vehicle
+        {
+            string color;
+        }
+    }
+}
