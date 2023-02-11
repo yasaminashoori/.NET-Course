744e60229be1085d9e33f598717034e42c7d007a
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-11 19:07:12 +0330

    Updated array obj / parametr

--- my-practices/46-parametrs-array-objects/Program.cs
+++ my-practices/46-parametrs-array-objects/Program.cs
@@ -6,7 +6,25 @@ namespace _46_parametrs_array_objects
     {
         static void Main(string[] args)
         {
-           
+           var myAnimal = new Animal("Snake");
+           ConfirmName(myAnimal, 0);
+           Console.WriteLine($"Legs for {myAnimal.Name} is just {myAnimal.Legs}");
+        }
+
+        public static void ConfirmName(Animal animal1, int legAnimal)
+        {
+            animal1.Legs = legAnimal;
+        }
+    }
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Legs { get; set; }
+
+        public Animal(string nameAnimal)
+        {
+            Name = nameAnimal;
         }
     }
 }
