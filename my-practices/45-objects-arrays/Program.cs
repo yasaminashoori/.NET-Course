e132f730f8ab2ac56a74cab1199ebeac60ed30cc
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-11 19:05:49 +0330

    Updated array objects

--- my-practices/45-objects-arrays/Program.cs
+++ my-practices/45-objects-arrays/Program.cs
@@ -7,22 +7,20 @@ namespace _45_arrays_objects
         static void Main(string[] args)
         {
             Animal[] zoo = { new Animal("bee"), new Animal("butterfly") };
-            // or write in this form =>  var zoo= new Animal[] { new Animal("bee"), new Animal("butterfly") };
-            // we call that anonymos objects
 
             foreach (var animal in zoo)
             {
-                Console.WriteLine(animal.Insects);
+                Console.WriteLine(animal.InsectsName);
             }
         }
 
         public class Animal 
         {
-            public string Insects { get; set; }
+            public string InsectsName { get; set; }
 
             public Animal(string kindesOfInsects)
             {
-                Insects = kindesOfInsects;
+                InsectsName = kindesOfInsects;
             }
         }
     }
