e5e956f91f67bae281a90a40790248247303c29f
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-12 16:30:38 +0330

    Every challenge code tested here (added CLI-BMI)

--- my-practices/workspace-code/Program.cs
+++ my-practices/workspace-code/Program.cs
@@ -6,18 +6,99 @@ namespace workspace_code
     {
         static void Main(string[] args)
         {
-            // Challenge 1 : calculate sqrt / the easiest wqy 
-            Console.WriteLine("Enter side A : ");
-            double sideA = Convert.ToDouble(Console.ReadLine());
+            // while (args[0] == "bmi" || args[0] == " ")
+            if (args[0] == "bmi")
+            {
+                if (args[1] == "--helps")
+                {
+                    HelpsCommand();
+                }
+                if (args[1] == "--version")
+                {
+                    VersionCommand();
+                }
+                if (args[0] == "bmi" && args.Length == 5)
+                {
+                    BmiCalculator(args);
+                }
+            }
+            else
+            {
+                ErrorMessage();
+            }
+        }
 
-            Console.WriteLine("Enter side B : ");
-            double sideB = Convert.ToDouble(Console.ReadLine());
+        static void ErrorMessage()
+        {
+            Console.WriteLine("Invalid Command :(  \n Use --helps switch to show help");
+        }
 
-            double result = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+        static void HelpsCommand()
+        {
+            Console.WriteLine("\n --height \t Your height (centimeters) ");
+            Console.WriteLine("\n --weight \t Your weight (kilograms) ");
+            Console.WriteLine("\n --version \t Show current version ");
+            Console.WriteLine("\n --helps \t Show command list ");
+        }
 
-            Console.WriteLine($"your result is : {result}");
+        static void VersionCommand()
+        {
+            Console.WriteLine("Current version is : 1.0");
+        }
 
-            // with class 
+        static void BmiCalculator(string[] args, double height, double weight)
+        {
+            // // var height = "";
+            // double heightNum;
+            // // var weight = "";
+            // double WeightNum;
+            // height = Convert.ToDouble(Console.ReadLine());
+            // weight = Convert.ToDouble(Console.ReadLine());
+            var bmi = weight / Math.Pow(height, 2);
+            if ((args[1] == "--height" && height == args[2] && args[3] == "--weight" && weight == args[4]))
+            {
+                if (bmi <= 18.4)
+                {
+                    Console.WriteLine($"Your BMI score is : \n {bmi} \n Your status is : \n Underweight");
+                }
+
+                else if (bmi >= 18.52 && bmi <= 24.9)
+                {
+                    Console.WriteLine($"Your BMI score is : \n {bmi} \n Your status is : \n Normal");
+                }
+
+                else if (bmi >= 25.0 && bmi <= 39.9)
+                {
+                    Console.WriteLine($"Your BMI score is : \n {bmi} \n Your status is : \n Overweight");
+                }
+
+                else
+                {
+                    Console.WriteLine($"Your BMI score is : \n {bmi} \n Your status is : \n Obese");
+                }
+            }
+            else if ((args[3] == "--height" && heightNum == args[4] && args[1] == "--weight" && WeightNum == args[2]))
+            {
+                if (bmi <= 18.4)
+                {
+                    Console.WriteLine($"Your BMI score is : \n {bmi} \n Your status is : \n Underweight");
+                }
+
+                else if (bmi >= 18.52 && bmi <= 24.9)
+                {
+                    Console.WriteLine($"Your BMI score is : \n {bmi} \n Your status is : \n Normal");
+                }
+
+                else if (bmi >= 25.0 && bmi <= 39.9)
+                {
+                    Console.WriteLine($"Your BMI score is : \n {bmi} \n Your status is : \n Overweight");
+                }
+
+                else
+                {
+                    Console.WriteLine($"Your BMI score is : \n {bmi} \n Your status is : \n Obese");
+                }
+            }
         }
     }
 }
