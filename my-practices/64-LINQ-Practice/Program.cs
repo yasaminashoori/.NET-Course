648a0a56180c7ef2babe51d81269f2ea3305e787
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-13 09:40:34 +0330

    Added second,right solution (act/score/city query)

--- my-practices/64-LINQ-Practice/Program.cs
+++ my-practices/64-LINQ-Practice/Program.cs
@@ -8,16 +8,10 @@ namespace _64_LINQ_Practice
         static void Main(string[] args)
         {
             var studentList = StudentDatabase.GetStudentsFromDb();
-            var activityStatus = studentList.Where(x => x.StudentActvity == true)
-                                .Select(x => x.StudentName);
-
-            var scoreMoreThan80 = studentList.Where(x => x.Score > 80)
-                                .Select(x => x.StudentName);
-
-            var liveInNyc = studentList.Where(x => x.StudentCity == "NYC")
-                                .Select(x => x.StudentName);
-
-            foreach (var item in activityStatus)
+            var studentsName = from Student in studentList
+                               where (Student.StudentActvity == true) && (Student.Score > 80) && (Student.StudentCity == "NYC")
+                               select Student.StudentName;
+            foreach (var item in studentsName)
             {
                 Console.WriteLine(item);
             }
