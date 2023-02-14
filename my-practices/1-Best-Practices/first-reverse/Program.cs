287716c39e4c968cba13f64a2bd65517e11ecafb
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-14 14:51:13 +0330

    Updated first reverse solution

---
+++ my-practices/1-Best-Practices/first-reverse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MainClass
+{
+
+    public static string FirstReverse(string str)
+    {
+
+        string reverse = String.Empty;
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            reverse += str[i];
+        }
+        return reverse;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter your favourite text : ");
+        Console.WriteLine(FirstReverse(Console.ReadLine()));
+    }
+}
