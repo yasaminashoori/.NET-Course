1f3d979eda78de53d0cd001d79878e13f0905cc3
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-15 13:23:02 +0330

    Lambda exp code reviewed

---
+++ my-practices/61-LINQ/Student.cs
@@ -0,0 +1,10 @@
+namespace _61_LINQ
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Score { get; set; }
+        public string StudentCity { get; set; }
+    }
+}
