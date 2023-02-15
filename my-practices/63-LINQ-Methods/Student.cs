722b616a569dc96dcedd2e1eff4cbffd5c13b1b7
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-15 13:20:30 +0330

    Update the LINQ methods and stDB program

---
+++ my-practices/63-LINQ-Methods/Student.cs
@@ -0,0 +1,19 @@
+namespace _61_LINQ
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Score { get; set; }
+        public string StudentCity { get; set; }
+    }
+    public class MinimalStudent
+    {
+        public string StudentName { get; set; }
+        public int Score { get; set; }  
+    }
+}
+
+
+
+
