a63f41a88825db593bbd843c5217d9efcefa87c8
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-15 13:21:08 +0330

    Solution reviewed

---
+++ my-practices/1-Best-Practices/first-factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace first_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstFactorial();
+        }
+
+        static void FirstFactorial()
+        {
+            int factorial = 1;
+            Console.WriteLine("enter your number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine($"the factorial of {number} is {factorial}");
+        }
+    }
+}
