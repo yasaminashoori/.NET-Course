40ec4283a3566fdefe9fcd80264bfcaa4e00a414
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-15 15:10:01 +0330

    Added first solution for Palindrome

---
+++ my-practices/1-Best-Practices/Palindrome-Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PalindromeChecker();
+        }
+
+        public static void PalindromeChecker()
+        {
+            Console.WriteLine("Enter an inteeger number : ");
+            var str = Console.ReadLine();
+            string reverse = String.Empty;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverse += str[i];
+            }
+            if (reverse == str)
+            {
+                Console.WriteLine($"{reverse} True, IT is a palindrome number.");
+            }
+            else
+            {
+                Console.WriteLine($"{reverse} Flase : not a palindrome number");
+            }
+        }
+    }
+}
