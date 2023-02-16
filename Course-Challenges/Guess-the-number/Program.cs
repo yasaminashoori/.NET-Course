01c7b08bc379fea1105837d0053c17426d9c77fe
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-16 19:54:53 +0330

    Updated the guess number solution

--- Course-Challenges/Guess-the-number/Program.cs
+++ Course-Challenges/Guess-the-number/Program.cs
@@ -7,31 +7,40 @@ namespace Guess_the_word
         static void Main(string[] args)
         {
             Random random = new Random();
-
-            int returnValue = random.Next(1, 100);
-
+            
             int Guess = 0;
 
-            Console.WriteLine("I am thinking of a number between 1-100.  Can you guess what it is?");
+            int round = 1;
 
-            while (Guess != returnValue)
+            var againPlay = false;
+            do
             {
-                Guess = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("I am thinking of a number between 1-100.  Can you guess what it is?");
+                int returnValue = random.Next(1, 100);
 
-                if (Guess < returnValue)
+                while (Guess != returnValue)
                 {
-                    Console.WriteLine("No, the number I am thinking of is higher than " + Guess + ". Can you guess what it is?");
+                    Guess = Convert.ToInt32(Console.ReadLine());
+
+                    if (Guess < returnValue)
+                    {
+                        Console.WriteLine("No, the number I am thinking of is higher than " + Guess + ". Can you guess what it is?");
+                        round++;
+                    }
+                    else if (Guess > returnValue)
+                    {
+                        Console.WriteLine("No, the number I am thinking of is lower than " + Guess + ". Can you guess what it is?");
+                        round++;
+                    }
                 }
-                else if (Guess > returnValue)
-                {
-                    Console.WriteLine("No, the number I am thinking of is lower than " + Guess + ". Can you guess what it is?");
-                }
+                Console.WriteLine("Well done! The answer was " + returnValue);
+                Console.WriteLine($"Finished in {round} rounds");
+                Console.WriteLine("DO you want to play more ? yes or no ");
+                againPlay = Console.ReadLine().ToUpper() == "NO";
 
-            }
+            } while (!againPlay);
 
-            Console.WriteLine("Well done! The answer was " + returnValue);
-            Console.ReadLine();
-
+            Console.WriteLine("Thanks for playing ^ --- ^ ");
         }
     }
 }
