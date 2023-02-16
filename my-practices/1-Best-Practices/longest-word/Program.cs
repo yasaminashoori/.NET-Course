52b4c645561e9de6f5141d0bd7a7552803170d89
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-16 19:51:07 +0330

    Update The solution for CW func

---
+++ my-practices/1-Best-Practices/longest-word/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class MainClass {
+  public static string LongestWord(string sen) { 
+      
+            Regex rgx = new Regex(@"[^\w\s]");
+            sen = rgx.Replace(sen,"");
+            string[] words = sen.Split(' ');
+        Console.Write("Your longest word is : ");
+            return words.OrderByDescending(x => x.Length).First();  
+  }
+
+  static void Main() {  
+    Console.WriteLine("Enter your favourite sentence :");
+    Console.WriteLine(LongestWord(Console.ReadLine()));
+  } 
+   
+}
