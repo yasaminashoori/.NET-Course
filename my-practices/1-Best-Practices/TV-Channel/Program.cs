b672a3ecda3d99e49c2316fc1e95256c1277f85c
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-16 14:38:30 +0330

    Updated TV challenge

---
+++ my-practices/1-Best-Practices/TV-Channel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TV_Channel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstChannel = 1;
+            var status = false;
+            do
+            {
+                Console.WriteLine($"We are in channel {firstChannel} do you like it ? ");
+                var userChannel = Console.ReadLine();
+                status = userChannel == "yes" ? true : false;
+                if (status)
+                {
+                    Console.WriteLine($"Channel {firstChannel} added to your favourites list.  ^-^ ");
+                }
+                else
+                {
+                    Console.WriteLine($" channel :  {++firstChannel}");
+                }
+            } while (status == false);
+        }
+    }
+}
