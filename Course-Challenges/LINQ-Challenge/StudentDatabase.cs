079add577e1b544551be0fd6fefc7f98419f38f4
yasaminashoori <yasaminashoori18@gmail.com>
2023-02-16 15:45:14 +0330

    LINQ practice answer added to Challenge

---
+++ Course-Challenges/LINQ-Challenge/StudentDatabase.cs
@@ -0,0 +1,19 @@
+using System.Linq;
+
+namespace LINQ_Challenge
+{
+    public class StudentDatabase
+    {
+        public static IQueryable<Student> GetStudentsFromDb()
+        {
+            return new[] {
+                new Student() {StudentId = 1, StudentName = "John Bross", Score = 82, StudentCity = "NYC", StudentActvity = true},
+                new Student() {StudentId = 2, StudentName = "Jasmine Curly", Score = 84, StudentCity = "NYC", StudentActvity = false},
+                new Student() {StudentId = 4, StudentName = "Ben Wild", Score = 65, StudentCity = "CA", StudentActvity = true},
+                new Student() {StudentId = 6, StudentName = "Sara Ride", Score = 81, StudentCity = "CA", StudentActvity = true},
+                new Student() {StudentId = 7, StudentName = "Bonnie Brooklyn", Score = 80, StudentCity = "LA", StudentActvity = false},
+                new Student() {StudentId = 8, StudentName = "Loren Gibson", Score = 88, StudentCity = "CA", StudentActvity = false},
+            }.AsQueryable();
+        }
+    }
+}
