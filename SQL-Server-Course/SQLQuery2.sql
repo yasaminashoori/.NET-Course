84b9cb0292c550d7ab453cd257ec89172eb909ca
yasaminashoori <yasaminashoori18@gmail.com>
2023-04-26 22:30:23 +0430

    Added queries for fill the table

---
+++ SQL-Server-Course/SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿Update tblStudent set Address =N'کرج' where id = 1
+
+Update tblStudent set Address =N'کرمانشاه' where Address =N'یزد'
+
+Update tblStudent set Address =N'کرمانشاه', IDCode = 1234
+ where Address =N'یزد'
+
+Delete From tblLesson Where Field= N'مهندسی'
+
+Delete From tblLesson Where Field= N'مهندسی' and ID not 
+in(Select LessonID from tblSection )
