1790977a14620d9035f9c806eaf154b80243f951
yasaminashoori <yasaminashoori18@gmail.com>
2023-05-20 00:31:12 +0430

    Add static class example

---
+++ C-Sharp-Review/Static-Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Static_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public static class FileLogger
+    {
+        private static readonly object lockObject = new object();
+        public static string FilePath
+        {
+            get; set;
+        }
+        public static void Log(string message)
+        {
+            lock (lockObject)
+            {
+                if(!string.IsNullOrEmpty(FilePath))
+                using (StreamWriter streamWriter = new StreamWriter(FilePath, true))
+                {
+                    streamWriter.WriteLine(message);
+                    streamWriter.Close();
+                }
+            }
+        }
+    }
+}
