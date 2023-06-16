163ec71adbff75eea6b76d3db7abb8eaf782d7e1
yasaminashoori <yasaminashoori18@gmail.com>
2023-06-17 00:16:26 +0430

    Add Uni Sorted numbers

---
+++ C-Sharp-Practices/1-Best-Practices/Uni/sorted numbers with if and else/sorted numbers with if and else/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter three numbers:");
+
+        Console.Write("Number 1: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Number 2: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Number 3: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        if (num1 >= num2 && num1 >= num3)
+        {
+            if (num2 >= num3)
+            {
+                Console.WriteLine(num1 + " > " + num2 + " > " + num3);
+            }
+            else
+            {
+                Console.WriteLine(num1 + " > " + num3 + " > " + num2);
+            }
+        }
+        else if (num2 >= num1 && num2 >= num3)
+        {
+            if (num1 >= num3)
+            {
+                Console.WriteLine(num2 + " > " + num1 + " > " + num3);
+            }
+            else
+            {
+                Console.WriteLine(num2 + " > " + num3 + " > " + num1);
+            }
+        }
+        else
+        {
+            if (num1 >= num2)
+            {
+                Console.WriteLine(num3 + " > " + num1 + " > " + num2);
+            }
+            else
+            {
+                Console.WriteLine(num3 + " > " + num2 + " > " + num1);
+            }
+        }
+    }
+}
