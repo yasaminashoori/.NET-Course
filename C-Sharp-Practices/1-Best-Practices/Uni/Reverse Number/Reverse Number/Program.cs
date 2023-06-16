d40a1f7f40de715b8782b506f1f63ecab8803793
yasaminashoori <yasaminashoori18@gmail.com>
2023-06-17 00:19:01 +0430

    Add Uni reverse number

---
+++ C-Sharp-Practices/1-Best-Practices/Uni/Reverse Number/Reverse Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        int reversedNumber = 0;
+
+        while (number != 0)
+        {
+            int digit = number % 10;
+            reversedNumber = (reversedNumber * 10) + digit;
+            number /= 10;
+        }
+
+        Console.WriteLine($"Reversed number: {reversedNumber}");
+
+        Console.ReadLine();
+    }
+}
