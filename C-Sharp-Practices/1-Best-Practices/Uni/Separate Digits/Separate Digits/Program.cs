ea47191a3d26170471b59d6ddbaf35f355c265c9
yasaminashoori <yasaminashoori18@gmail.com>
2023-06-17 00:19:19 +0430

    Add Uni Seperate Digits

---
+++ C-Sharp-Practices/1-Best-Practices/Uni/Separate Digits/Separate Digits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digits of the number:");
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            Console.WriteLine(digit);
+            number /= 10;
+        }
+
+        Console.ReadLine();
+    }
+}
