a419322d4b4f2bbd08f757d4a143e93531a80d24
yasaminashoori <yasaminashoori18@gmail.com>
2023-06-17 00:18:46 +0430

    Add Uni Number Length

---
+++ C-Sharp-Practices/1-Best-Practices/Uni/Number length/Number length/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number, j = 0;
+        number = int.Parse(Console.ReadLine());
+        for (int i = number; i != 0; i /= 10) ++j;
+        Console.WriteLine(j);
+    }
+}
