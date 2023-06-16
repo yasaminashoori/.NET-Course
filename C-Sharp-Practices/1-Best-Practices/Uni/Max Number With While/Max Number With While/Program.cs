4e4543a29bae8fb95caf26a863dc88d53b3c7431
yasaminashoori <yasaminashoori18@gmail.com>
2023-06-17 00:18:30 +0430

    Add Uni Max Number with While

---
+++ C-Sharp-Practices/1-Best-Practices/Uni/Max Number With While/Max Number With While/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int max, number;
+        max = -1;
+        number = int.Parse(System.Console.ReadLine());
+        while (number > 0)
+        {
+            if (number > max)
+                max = number;
+            number = int.Parse(System.Console.ReadLine());
+
+        }
+        if (max > 0)
+            Console.WriteLine(max);
+        else
+            System.Console.WriteLine("meow");
+
+
+
+    }
+}
+
