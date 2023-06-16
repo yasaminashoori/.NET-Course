f61af9c51ea69756a76164dcb76a85a15a093110
yasaminashoori <yasaminashoori18@gmail.com>
2023-06-17 00:18:07 +0430

    Add Uni max Number

---
+++ C-Sharp-Practices/1-Best-Practices/Uni/Max Number with for/Max Number with for/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        int max = -1, number;
+        for (number = int.Parse(System.Console.ReadLine()); number >= 0; number = int.Parse(System.Console.ReadLine()))
+            if (number > max)
+                max = number;
+        if (max >= 0)
+            System.Console.WriteLine(max);
+        else
+            System.Console.WriteLine("Meow");
+
+    }
+}
