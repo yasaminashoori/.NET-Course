c427077bfbe0416dacfa1151785eca4f646a3d55
yasaminashoori <yasaminashoori18@gmail.com>
2023-08-07 23:40:10 +0430

    Add code 617 quera

---
+++ C-Sharp-Practices/C-Sharp-Quera/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace C_Sharp_Quera;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ENter a NUmber:");
+        string number = Console.ReadLine();
+        char[] numberArr = number.ToCharArray();
+        Array.Reverse(numberArr);
+        string b = new string(numberArr);
+
+        var result = number == b? "YES" : "NO";
+        Console.WriteLine(result);
+    }
+}
