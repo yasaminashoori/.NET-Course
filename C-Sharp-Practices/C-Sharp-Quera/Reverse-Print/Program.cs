a1dc145427d38ac1edfcd2952defa344cfba0616
yasaminashoori <yasaminashoori18@gmail.com>
2023-08-08 00:01:07 +0430

    Add Khoshghalb quera

---
+++ C-Sharp-Practices/C-Sharp-Quera/Reverse-Print/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Reverse_Print;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberofInput = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberofInput; i++)
+        {
+            Console.WriteLine("man khoshghlab hastam");
+        }
+    }
+}
