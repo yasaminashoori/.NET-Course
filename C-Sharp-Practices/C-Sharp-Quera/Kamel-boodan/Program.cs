274fdafabcd06c7c536a817d0e232c3ab63fdca1
yasaminashoori <yasaminashoori18@gmail.com>
2023-08-10 02:15:48 +0430

    Add Kamel boodan ( second question second dat)

---
+++ C-Sharp-Practices/C-Sharp-Quera/Kamel-boodan/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kamel_boodan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENter a number:");
+            int input1 = convert.TOint32(console.ReadLine());
+
+            // var result = input1 / 2 + input1 / 3 + input1
+            for (int i = 1; i < input1 ; i++)
+            {
+                result = input1 / i;
+                result = 
+            }
+
+        }
+    }
+}
