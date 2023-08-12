3079bbaab9c34e304e3342cae1062ef200222e14
yasaminashoori <yasaminashoori@gmail.com>
2023-08-12 16:18:59 +0330

    Add Tarikh Tavalod Quera

---
+++ C-Sharp-Practices/C-Sharp-Quera/Tarikh-Tavalod/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tarikh_Tavalod;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a 4 digits number");
+        var tarikh = Console.ReadLine()
+        
+    }
+}
