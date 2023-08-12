e1c3f7fe605f1b7ee9e98530833f0211b05c438e
yasaminashoori <yasaminashoori@gmail.com>
2023-08-12 14:14:11 +0330

    Add HaftSin quera

--- C-Sharp-Practices/C-Sharp-Quera/Adad-Khod-Maghloub/Program.cs
+++ C-Sharp-Practices/C-Sharp-Quera/Adad-Khod-Maghloub/Program.cs
@@ -4,13 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("ENter a NUmber:");
-        string number = Console.ReadLine();
-        char[] numberArr = number.ToCharArray();
-        Array.Reverse(numberArr);
-        string b = new string(numberArr);
-
-        var result = number == b? "YES" : "NO";
-        Console.WriteLine(result);
+        Console.WriteLine("Hell");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("ENter a NUmber:");
+//         string number = Console.ReadLine();
+//         char[] numberArr = number.ToCharArray();
+//         Array.Reverse(numberArr);
+//         string b = new string(numberArr);
+
+//         var result = number == b? "YES" : "NO";
+//         Console.WriteLine(result);
---
+++ C-Sharp-Practices/C-Sharp-Quera/Em-Sin/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography;
+
+namespace Em_Sin;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string haftSin = new string[] {"samanou", "sabzeh", "sir", "serkeh", "senjed", "sekeh", "sib"};
+        if (user > 0)
+        {
+            
+        }
+
+    }
+}
