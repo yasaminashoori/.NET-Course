bf01a28d5dc925b7fb1915ab98ce3c37cf435b98
yasaminashoori <yasaminashoori@gmail.com>
2023-08-12 16:06:00 +0330

    Add changes

--- C-Sharp-Practices/C-Sharp-Quera/Em-Sin/Program.cs
+++ C-Sharp-Practices/C-Sharp-Quera/Em-Sin/Program.cs
@@ -6,11 +6,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        string haftSin = new string[] {"samanou", "sabzeh", "sir", "serkeh", "senjed", "sekeh", "sib"};
-        if (user > 0)
-        {
-            
-        }
+        string haftSin = new string[] { "samanou", "sabzeh", "sir", "serkeh", "senjed", "sekeh", "sib" };
+        Console.WriteLine("Enter a number between 1 to 7");
+        var userNumber = Console.ReadLine().ToLower();
 
+        if (userNumber >= 1)
+        {
+            for (int i = 0; i < haftSin.Length; i++)
+            {
+                foreach (var item in haftSin)
+                {
+                    haftSin[item] = userNumber;
+                    Console.WriteLine(item);
+                }
+            }
+        }
     }
 }
