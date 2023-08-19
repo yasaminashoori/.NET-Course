45b7514d5783cfd742bd498b4567004de8e1d043
yasaminashoori <yasaminashoori18@gmail.com>
2023-08-19 16:33:05 +0330

    add first part of prime number

--- C-Sharp-Practices/C-Sharp-Quera/adad-aval/Program.cs
+++ C-Sharp-Practices/C-Sharp-Quera/adad-aval/Program.cs
@@ -8,17 +8,7 @@ class Program
        var secondNumber = int.Parse(Console.ReadLine());
        for (int i = firstNumber; i <= secondNumber; i++)
        {
-            var adadAval = i;
-            if (firstNumber % 1 == 0 && secondNumber % 1 == 0 && i % 1 == 0 && firstNumber % firstNumber == 0  && secondNumber % secondNumber == 0 && i % i == 0)
-
-            {
-                Console.WriteLine("there isn't any prime numbr");
-            }
-            else
-            {
-                Console.WriteLine("The result: ");
-                Console.WriteLine($"{firstNumber} {i} {secondNumber}");
-            }
+            
        }
     }
 }
